2097d0d866344088c72fc2754b01ed16b598dfd0
Olly <data19101988@gmail.com>
2019-05-09 00:46:54 +0300

    fancybox for item preview. продумать - ибо у нас есть две кнопки на боксе, по которому кликаешь/наводишь мышью. если на них тыкаешь то отрабатывает вызов fancybox

---
+++ js/catalog.js
@@ -0,0 +1,89 @@
+const siemas = document.querySelectorAll('.siema');
+
+// this is fairly new way of looping through DOM Elements
+// More about ithere: https://pawelgrzybek.com/loop-through-a-collection-of-dom-elements/
+// For better compatibility I suggest using for loop
+for (const siema of siemas) {
+  var id = siema.id;
+  const mySiema = new Siema({
+    selector: siema,
+    duration: 600,
+    easing: 'cubic-bezier(.17,.67,.68,1.73)',
+    perPage: 1,
+    startIndex: 0,
+    draggable: true,
+    multipleDrag: true,
+    threshold: 20,
+    rtl: false
+  });
+
+  // Add a function that generates pagination to prototype
+  Siema.prototype.addPagination = function() {
+
+    var siemaInstance = this.selector.id;
+
+    switch (siemaInstance) {
+      case "relatedStuff":
+        for (let i = 0; i < this.innerElements.length; i++) {
+          const span = $('<span />').attr('data-slide', i).appendTo('#relatedSiemaNav');
+          i == 0 ? span.addClass('active') : null;
+          span.on('click', () => this.goTo(i,() => setActive(span)));
+        }
+        break;
+      case "similarStuff":
+        for (let i = 0; i < this.innerElements.length; i++) {
+          const span = $('<span />').attr('data-slide', i).appendTo('#similarSiemaNav');
+            i == 0 ? span.addClass('active') : null;
+          span.on('click', () => this.goTo(i,() => setActive(span)));
+
+        }
+        break;
+      default:
+        console.log('no siema instances');
+    }
+
+  }
+
+  // Trigger pagination creator
+  mySiema.addPagination();
+  function setActive(span) {
+    span.siblings().removeClass('active');
+    span.addClass('active');
+  }
+}
+
+/////////////////////////////////////////////////////
+$('div[id^="cti_"]').each(function () {
+
+  $(this).click(function (e) {
+
+  var elementIndex = this.id,
+            target = '#preview_' + elementIndex;
+  $.fancybox.open({
+    src  : target,
+    type : 'inline',
+    opts : {
+      afterShow : function( instance, current ) {
+        console.info( target );
+      }
+    }
+  });
+  // $.fancybox.open($('#' + elementIndex + 'preview'), {
+  //   touch: false
+  // });
+});
+})
+
+// $('div[id^="cti_"]').on('click', function() {
+//   var elementIndex = this.id,
+//             target = '#preview_' + elementIndex + '';
+//   $.fancybox.open({
+//     src  : target,
+//     type : 'inline',
+//     opts : {
+//       afterShow : function( instance, current ) {
+//         console.info( target );
+//       }
+//     }
+//   });
+// });
