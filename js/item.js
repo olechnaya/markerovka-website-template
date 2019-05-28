57a59cf94f3b878b35e1928cb229076c07a2ade3
Olly <data19101988@gmail.com>
2019-05-29 02:16:06 +0300

    IT WORKS!! tabs height count and redrawing for minimal height

--- js/item.js
+++ js/item.js
@@ -1,51 +1,65 @@
 $(function() {
   $(document).ready(function() {
-      $('body').fadeIn(1000);
+    $('body').fadeIn(1000);
   });
 
   jcf.setOptions('Select', {
     wrapNative: false,
     fakeDropInBody: false,
-	  useCustomScroll: true
-});
-    jcf.replaceAll();
+    useCustomScroll: true
+  });
+  jcf.replaceAll();
 
-    $('[data-fancybox="gallery"]').fancybox({
-toolbar: false
-});
+  $('[data-fancybox="gallery"]').fancybox({
+    toolbar: false
+  });
 
-    $('.miniature').on('click', function(e) {
-        e.preventDefault();
-        var imgSource = $(this).attr('href'),
-            $targetElem = $('#photoDecor').find('#'+ $(this).attr("data-id-link"));
+  $('.miniature').on('click', function(e) {
+    e.preventDefault();
+    var imgSource = $(this).attr('href'),
+      $targetElem = $('#photoDecor').find('#' + $(this).attr("data-id-link"));
 
-        $('#photoDecor').find('.large').css("zIndex", '')
-        $targetElem.css('zIndex', 2);
-        $(this).addClass('active').siblings().removeClass('active');
+    $('#photoDecor').find('.large').css("zIndex", '')
+    $targetElem.css('zIndex', 2);
+    $(this).addClass('active').siblings().removeClass('active');
+  });
+
+  var heights = new Array();
+
+  setTimeout(function() {
+    setTabsHeights();
+  }, 650);
+
+  function setTabsHeights() {
+    $('.tab-content .tab-pane').each(function() {
+      heights.push($(this).height())
+    });
+    $('.tab-content .tab-pane').css('height', Math.min.apply(null, heights))
+  }
+
+  $('#itemTab a').on('shown.bs.tab', function(e) {
+
+    e.preventDefault()
+    var activeTab = $(e.target),
+      previousTab = $(e.relatedTarget),
+      activeTabPane = $(activeTab.attr('href')),
+      previousTabPane = $(previousTab.attr('href')),
+      minHeight = Math.min.apply(null, heights);
+
+    activeTab.css({
+      'top': 0
     });
 
-    // $('#itemTab a').on('click', function (e) {
-    //   e.preventDefault()
-    //   $(this).tab('show')
-    // })
-    $('#itemTab a').on('shown.bs.tab', function(e){
-    e.preventDefault()
-      var activeTab = $(e.target),
-          previousTab = $(e.relatedTarget),
-          activeTabPane = $(activeTab.attr('href')),
-          previousTabPane = $(previousTab.attr('href'));
+    previousTab.removeAttr('style');
 
-          activeTab.css({
-            'top':0
-          });
-          previousTab.removeAttr( 'style' );
-
-          activeTabPane.css({
-            'zIndex': 7,
-            'background-color': '#fffffe',
-            'top':0,
-            'width':'100%'
-          });
-          previousTabPane.removeAttr( 'style' );
+    activeTabPane.css({
+      'height': 'auto',
+      'zIndex': 7,
+      'background-color': '#fffffe',
+      'top': 0,
+      'width': '100%'
+    });
+    previousTabPane.removeAttr('style');
+    previousTabPane.css('height', minHeight);
   });
 });
