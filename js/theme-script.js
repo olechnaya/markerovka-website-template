03f065dd4bd8e52b7c25c0ba3d925c6da54830e0
Olly <data19101988@gmail.com>
2019-05-29 22:40:45 +0300

    Siema slider working
    several instances usage
    buttons styles changing
    images changing

--- js/theme-script.js
+++ js/theme-script.js
@@ -90,8 +90,30 @@ jQuery(document).ready(function($) {
     });
   });
 
+  function onChangeCallback() {
+
+    // styling navigation buttons depending on selected slide
+    $(this.selector).parents('.btn-sticker')
+                    .find('.navigation button')
+                    .removeClass('active');
+
+    $($(this.selector).parents('.btn-sticker')
+                      .find('.navigation button')[this.currentSlide])
+                      .addClass('active');
+
+    // showing image depending on selected slide
+    $(this.selector).parents('.annual-addings')
+                    .find('.img-container')
+                    .removeClass('active');
+
+    $($(this.selector).parents('.annual-addings')
+                      .find('.img-container')[this.currentSlide])
+                      .addClass('active');
+  }
+
+  // govnocode is starting
   const annualadding1_sliderSiema = new Siema({
-    selector: '#mainOfficeRouteSiemaSlider .siema',
+    selector: '#addingsOf2018 .siema',
     duration: 200,
     easing: 'ease-out',
     perPage: 1,
@@ -99,22 +121,80 @@ jQuery(document).ready(function($) {
     draggable: true,
     multipleDrag: true,
     threshold: 20,
-    loop: true,
-    rtl: false//,
-    //onChange: onChangeCallback,
+    loop: false,
+    rtl: false,
+    onChange: onChangeCallback,
   });
   Siema.prototype.addPagination = function(instance) {
-    var test = document.createElement('div');
-    //  debugger;
-    test.setAttribute("id", $(instance).parent().attr('id') + "Nav");
-    test.setAttribute("class", "navigation");
-    this.selector.appendChild(test);
+    var navDiv = document.createElement('div');
+
+    navDiv.setAttribute("id", $(instance).parents('.btn-sticker').attr('id') + "Nav");
+    navDiv.setAttribute("class", "navigation");
+
+    $(instance).parents('.btn-sticker').append(navDiv);
 
     for (let i = 0; i < this.innerElements.length; i++) {
       const btn = document.createElement('button');
       if (i == 0) btn.setAttribute("class", "active");
       btn.addEventListener('click', () => this.goTo(i));
-      test.appendChild(btn);
+      navDiv.appendChild(btn);
     }
   }
+
+  annualadding1_sliderSiema.addPagination(annualadding1_sliderSiema.selector);
+
+// govnocode is continuing
+  const annualadding2_sliderSiema = new Siema({
+    selector: '#addingsOf2017 .siema',
+    duration: 200,
+    easing: 'ease-out',
+    perPage: 1,
+    startIndex: 0,
+    draggable: true,
+    multipleDrag: true,
+    threshold: 20,
+    loop: false,
+    rtl: false,
+    onChange: onChangeCallback,
+  });
+
+  annualadding2_sliderSiema.addPagination(annualadding2_sliderSiema.selector);
+
+  // govnocode is continuing
+    const annualadding3_sliderSiema = new Siema({
+      selector: '#addingsOf2016 .siema',
+      duration: 200,
+      easing: 'ease-out',
+      perPage: 1,
+      startIndex: 0,
+      draggable: true,
+      multipleDrag: true,
+      threshold: 20,
+      loop: false,
+      rtl: false,
+      onChange: onChangeCallback,
+    });
+
+    annualadding3_sliderSiema.addPagination(annualadding3_sliderSiema.selector);
+
+    // govnocode is continuing
+      const annualadding4_sliderSiema = new Siema({
+        selector: '#addingsOf2015 .siema',
+        duration: 200,
+        easing: 'ease-out',
+        perPage: 1,
+        startIndex: 0,
+        draggable: true,
+        multipleDrag: true,
+        threshold: 20,
+        loop: false,
+        rtl: false,
+        onChange: onChangeCallback,
+      });
+
+      annualadding4_sliderSiema.addPagination(annualadding4_sliderSiema.selector);
+
+      //TODO:
+      // declare siema object for 
+      // array ['addingsOf2015','addingsOf2016','addingsOf2017', 'addingsOf2018','addingsOf2019']
 });
