98ef03212cd34999134e60b6fc5a8c53077c5805
Олька <data19101988@gmail.com>
2019-06-14 14:52:34 +0300

    carousel in catalog replaced

--- js/catalog.js
+++ js/catalog.js
@@ -1,63 +1,11 @@
-const siemas = document.querySelectorAll('.siema');
 
-// this is fairly new way of looping through DOM Elements
-// More about ithere: https://pawelgrzybek.com/loop-through-a-collection-of-dom-elements/
-// For better compatibility I suggest using for loop
-for (const siema of siemas) {
-  var id = siema.id;
-  const mySiema = new Siema({
-    selector: siema,
-    duration: 600,
-    easing: 'cubic-bezier(.17,.67,.68,1.73)',
-    perPage: 1,
-    startIndex: 0,
-    draggable: true,
-    multipleDrag: true,
-    threshold: 20,
-    rtl: false
-  });
+$('#relatedStuff, #similarStuff').on('slide.bs.carousel', function(e) {
+  var slideTo = $(e.relatedTarget).index(),
+      indicators = $(this).prev().find('.carousel-indicators span');
 
-  // Add a function that generates pagination to prototype
-  Siema.prototype.addPagination = function() {
-
-    var siemaInstance = this.selector.id;
-
-    switch (siemaInstance) {
-      case "relatedStuff":
-        for (let i = 0; i < this.innerElements.length; i++) {
-          const span = $('<span />').attr('data-slide', i).appendTo('#relatedSiemaNav');
-          i == 0 ? span.addClass('active') : null;
-          span.on('click', () => this.goTo(i,() => setActive(span)));
-        }
-        break;
-      case "similarStuff":
-        for (let i = 0; i < this.innerElements.length; i++) {
-          const span = $('<span />').attr('data-slide', i).appendTo('#similarSiemaNav');
-            i == 0 ? span.addClass('active') : null;
-          span.on('click', () => this.goTo(i,() => setActive(span)));
-
-        }
-        break;
-      default:
-        console.log('no siema instances');
-    }
-
-  }
-
-  // Trigger pagination creator
-  mySiema.addPagination();
-  function setActive(span) {
-    span.siblings().removeClass('active');
-    span.addClass('active');
-  }
-}
-
-/////////////////////////////////////////////////////
-// $('div[id^="cti_"]').each(function () {
-//   var elementIndex = this.id,
-//             target = '#preview_' + elementIndex;
-// })
-
+    indicators.removeClass('active');
+    $($(indicators)[slideTo]).addClass('active');
+});
 
 $('.product').tooltipster({
     contentCloning: true,
--- js/index.js
+++ js/index.js
@@ -2,17 +2,12 @@ jQuery(document).ready(function($) {
   "use strict";
 
   $(function() {
-    $('.carousel').carousel();
-
-    $('#addingsOf2017').on('slide.bs.carousel', function(e) {
+        $('#addingsOf2017').on('slide.bs.carousel', function(e) {
 
       var slideTo = $(e.relatedTarget).index();
 
       $(this).parents('.annual-addings').find('.img-container').removeClass('active');
       $($(this).parents('.annual-addings').find('.img-container')[slideTo]).addClass('active');
-    })
-
-
+    });
   });
-
 });
