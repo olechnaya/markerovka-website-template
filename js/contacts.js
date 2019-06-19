45d3aa6ced509a7eb464a1974909cf40e3ee8616
Олька <data19101988@gmail.com>
2019-06-19 22:19:59 +0300

    carousel in contacts replaced

--- js/contacts.js
+++ js/contacts.js
@@ -13,74 +13,14 @@ $(function() {
     e.preventDefault();
     var targetDiv = $(this).attr('href'),
         container = $(this).parents('.office-route'),
-        switches = $(this).parents('.office-route').find('.switches a');
-
+        switches = $(this).parents('.switches').find(' a');
     if ($(this).hasClass('active')) return;
 
     switches.removeClass('active');
-
-    container.children('div').removeClass('active');
+    container.children('.route-item').removeClass('active');
 
     $(targetDiv).addClass('active')
     $(this).addClass('active');
   })
 
-  function onChangeCallback() {
-    $(this.selector).parent().find('.navigation button').removeClass('active')
-    $($(this.selector).parent().find('.navigation button')[this.currentSlide]).addClass('active')
-  }
-
-  const sae_sliderSiema = new Siema({
-    selector: '#mainOfficeRouteSiemaSlider .siema',
-    duration: 200,
-    easing: 'ease-out',
-    perPage: 1,
-    startIndex: 0,
-    draggable: true,
-    multipleDrag: true,
-    threshold: 20,
-    loop: true,
-    rtl: false,
-    onChange: onChangeCallback,
-  });
-  $('#mainOfficeRouteSiemaSlider').on('click', '.prev', () => sae_sliderSiema.prev());
-  $('#mainOfficeRouteSiemaSlider').on('click', '.next', () => sae_sliderSiema.next());
-
-  Siema.prototype.addPagination = function(instance) {
-    var test = document.createElement('div');
-    //  debugger;
-    test.setAttribute("id", $(instance).parent().attr('id') + "Nav");
-    test.setAttribute("class", "navigation");
-    this.selector.appendChild(test);
-
-    for (let i = 0; i < this.innerElements.length; i++) {
-      const btn = document.createElement('button');
-      if (i == 0) btn.setAttribute("class", "active");
-      btn.addEventListener('click', () => this.goTo(i));
-      test.appendChild(btn);
-    }
-  }
-
-  // Trigger pagination creator
-  sae_sliderSiema.addPagination(sae_sliderSiema.selector);
-
-  //  Second instance of carousel
-  const sae2_sliderSiema = new Siema({
-    selector: '#nagornayaOfficeRouteSiemaSlider .siema',
-    duration: 200,
-    easing: 'ease-out',
-    perPage: 1,
-    startIndex: 0,
-    draggable: true,
-    multipleDrag: true,
-    threshold: 20,
-    loop: true,
-    rtl: false,
-    onChange: onChangeCallback,
-  });
-  $('#nagornayaOfficeRouteSiemaSlider').on('click', '.prev', () => sae2_sliderSiema.prev());
-  $('#nagornayaOfficeRouteSiemaSlider').on('click', '.next', () => sae2_sliderSiema.next());
-
-  // Trigger pagination creator
-  sae2_sliderSiema.addPagination(sae2_sliderSiema.selector);
 });
